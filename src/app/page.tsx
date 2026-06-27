f1f0ae657d34dc24997d2aa70632d9fcf6da7609
Jason8908 <jasonsu894@gmail.com>
2026-06-26 23:31:37 -0400

    about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import { Header } from "@/components/navigation/Header";
+import { Container } from "@/components/layout/Container";
+import { About } from "@/components/sections/About";
 
 export default function Home() {
   return (
     <main className="min-h-screen w-full">
       <Header />
+      <Container>
+        <About />
+      </Container>
     </main>
   );
 }
